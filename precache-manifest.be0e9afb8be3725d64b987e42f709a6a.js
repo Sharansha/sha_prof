9c66f8031858ea0508282433a292d44fbdbfd251
Sharansha <sharansha375@gmail.com>
2020-05-25 20:02:46 +0530

    Updates

---
+++ precache-manifest.be0e9afb8be3725d64b987e42f709a6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a539ff8568f8e260513b",
+    "url": "/Sharan_profile/static/css/main.09d54a59.chunk.css"
+  },
+  {
+    "revision": "a539ff8568f8e260513b",
+    "url": "/Sharan_profile/static/js/main.dbc30ec7.chunk.js"
+  },
+  {
+    "revision": "61c642ff4bdbb5ca4920",
+    "url": "/Sharan_profile/static/js/runtime~main.a437887b.js"
+  },
+  {
+    "revision": "aa44596a64f161cf2dc0",
+    "url": "/Sharan_profile/static/css/2.b2d64cdf.chunk.css"
+  },
+  {
+    "revision": "aa44596a64f161cf2dc0",
+    "url": "/Sharan_profile/static/js/2.8616273d.chunk.js"
+  },
+  {
+    "revision": "008aacaa5fcdca065e9b0f0aff83a168",
+    "url": "/Sharan_profile/static/media/icon.008aacaa.svg"
+  },
+  {
+    "revision": "b16c5505b913c84aba16ed595bd9d8be",
+    "url": "/Sharan_profile/index.html"
+  }
+];
